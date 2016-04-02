fdfee4652bf48601d36105f0c55c0319bc6927b1
sujoysaha777 <sujoy_saha@tamu.edu>
2016-04-02 05:45:33 +0000

    finale

--- spec/models/movies_spec.rb
+++ spec/models/movies_spec.rb
@@ -37,11 +37,54 @@ describe Movie do
       end
     end
   end
+  
   describe "#all_ratings" do 
     it 'should return array ["G", "PG", "PG-13", "NC-17", "R"]' do
       Movie.stub(:all_ratings).and_return(["G", "PG", "PG-13", "NC-17", "R"]) 
       expect(Movie.all_ratings).to eq(["G", "PG", "PG-13", "NC-17", "R"])
     end
   end
+end
+
+#require 'spec_helper'
+
+RSpec.describe MoviesController, :type => :controller do
+
+  
+
+  describe 'UPDATE DIRECTOR' do
+   
+    it 'should call update_attributes and redirect' do
+      @mov1 = double('Movie', :title => 'Hunger Games', :director => 'Nora', :id => '21')
+      allow(Movie).to receive(:find).with('21').and_return(@mov1)
+      expect(@mov1).to receive(:update_attributes!).and_return(true)
+      put :update, {:id => '21', :movie => @mov1}
+      expect(response).to redirect_to(movie_path(@mov1))
+    end
+  end
+  
+  describe 'HOMEPAGE' do
+    it 'render HOMEPAGE' do
+      get :index
+      expect(response).to render_template('index')
+    end
+  end
+  
+  describe 'CREATE/DELETE' do
+    it 'DELETE' do
+      movie = double('Movie', :title => 'Hunger Games 2', :director => 'Nora', :id => '23')
+      allow(Movie).to receive(:find).with('23').and_return(movie)
+      expect(movie).to receive(:destroy)
+      delete :destroy, {:id => '23'}
+    end
+    it 'CREATE' do
+      movie = double('Movie', :title => 'Hunger Games', :director => 'Nora', :id => '2')
+      allow(Movie).to receive(:create).and_return(movie)
+      post :create, {:id => '2'}
+    end
+    
+  end
+
+
 end
 
